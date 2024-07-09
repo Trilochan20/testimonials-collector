a83d1cc0634b2864d61404dfbe251c7ec0221c2c
Trilochan20 <satapathytrilochan20@yahoo.com>
2024-07-09 23:58:58 +0530

    walkthrough mobile layout fixed

--- components/homepage/Walkthrough.tsx
+++ components/homepage/Walkthrough.tsx
@@ -6,7 +6,7 @@ const WalkthroughSection: React.FC = () => {
       <div className=" py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl  lg:py-20 md:mb-12 px-8">
         <div className="grid gap-10 lg:grid-cols-2">
           <div className="lg:pr-10 order-2 lg:order-1">
-            <h5 className="mb-4 text-4xl font-extrabold leading-none text-slate-900 dark:text-slate-100">
+            <h5 className="mb-4 text-4xl font-bold tracking-wide leading-none text-slate-900 dark:text-slate-100">
               The quick, brown fox
               <br className="hidden md:block" />
               jumps over{" "}
@@ -66,8 +66,118 @@ const WalkthroughSection: React.FC = () => {
               alt=""
             />
           </div>
-          <div className="lg:text-right lg:pr-10 order-2 lg:order-1">
-            <h5 className="mb-4 text-4xl font-extrabold leading-none text-slate-900 dark:text-slate-100">
+
+          <div className="lg:text-right  order-2 lg:order-1">
+            <h5 className="mb-4 text-4xl font-bold tracking-wide leading-none text-slate-900 dark:text-slate-100">
+              The quick, brown fox
+              <br className="hidden md:block" />
+              jumps over{" "}
+              <span className="inline-block text-deep-purple-accent-400">
+                a lazy dog
+              </span>
+            </h5>
+            <p className="mb-6 text-slate-900 dark:text-slate-100">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae explicabo. Sed ut perspiciatis unde omnis iste natus error
+              sit voluptatem totam rem aperiam, eaque ipsa quae explicabo.
+            </p>
+            <hr className="mb-5 border-gray-300" />
+            <div className="flex items-center lg:justify-end space-x-4">
+              <a
+                href="/"
+                className="text-slate-900 dark:text-slate-100 dark:text-slate-100transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <FaTwitter size={20} />
+              </a>
+              <a
+                href="/"
+                className="text-slate-900 dark:text-slate-100 transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <FaInstagram size={24} />
+              </a>
+              <a
+                href="/"
+                className="text-slate-900 dark:text-slate-100 transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <FaFacebook size={20} />
+              </a>
+              <a
+                href="/"
+                className="text-slate-900 dark:text-slate-100 transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <FaYoutube size={24} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className=" py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl  lg:py-20 md:mb-12 px-8">
+        <div className="grid gap-10 lg:grid-cols-2">
+          <div className="lg:pr-10 order-2 lg:order-1">
+            <h5 className="mb-4 text-4xl font-bold tracking-wide leading-none text-slate-900 dark:text-slate-100">
+              The quick, brown fox
+              <br className="hidden md:block" />
+              jumps over{" "}
+              <span className="inline-block text-deep-purple-accent-400">
+                a lazy dog
+              </span>
+            </h5>
+            <p className="mb-6 text-slate-900 dark:text-slate-100">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae explicabo. Sed ut perspiciatis unde omnis iste natus error
+              sit voluptatem totam rem aperiam, eaque ipsa quae explicabo.
+            </p>
+            <hr className="mb-5 border-gray-300" />
+            <div className="flex items-center space-x-4">
+              <a
+                href="/"
+                className="text-slate-900 dark:text-slate-100 dark:text-slate-100transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <FaTwitter size={20} />
+              </a>
+              <a
+                href="/"
+                className="text-slate-900 dark:text-slate-100 transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <FaInstagram size={24} />
+              </a>
+              <a
+                href="/"
+                className="text-slate-900 dark:text-slate-100 transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <FaFacebook size={20} />
+              </a>
+              <a
+                href="/"
+                className="text-slate-900 dark:text-slate-100 transition-colors duration-300 hover:text-deep-purple-accent-400"
+              >
+                <FaYoutube size={24} />
+              </a>
+            </div>
+          </div>
+          <div className="order-1 lg:order-2">
+            <img
+              className="object-cover w-full h-56 rounded shadow-lg sm:h-96"
+              src="https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <div className=" py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl  lg:py-20 md:mb-12 px-8">
+        <div className="grid gap-10 lg:grid-cols-2">
+          <div className="order-2 lg:order-1">
+            <img
+              className="object-cover w-full h-56 rounded shadow-lg sm:h-96"
+              src="https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+              alt=""
+            />
+          </div>
+
+          <div className="lg:text-right  order-2 lg:order-1">
+            <h5 className="mb-4 text-4xl font-bold tracking-wide leading-none text-slate-900 dark:text-slate-100">
               The quick, brown fox
               <br className="hidden md:block" />
               jumps over{" "}
