8a5fff7d697b44fcc95c3e29fa488603f344c834
Trilochan20 <satapathytrilochan20@yahoo.com>
2024-08-12 23:02:02 +0530

    dashboard ui started

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashbaord() {
+  return (
+    <>
+      <section className="pt-24 pl-6 max-w-7xl m-auto w-full">
+        <p>here goes content</p>
+      </section>
+    </>
+  );
+}
